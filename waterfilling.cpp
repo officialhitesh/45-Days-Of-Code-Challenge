2221a0bd49bf4c3741bdafb5c64c09cc2be048bd
Hitesh Kumar <hiteshbksc@gmail.com>
2024-10-02 15:14:36 +0530

    Day 5
    level=easy

---
+++ waterfilling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T;
+    cin >> T;  
+    for (int i = 0; i < T; i++) {
+        int B1, B2, B3;
+        cin >> B1 >> B2 >> B3;  
+        int empty_count = (B1 == 0) + (B2 == 0) + (B3 == 0);
+        if (empty_count >= 2) {
+            cout << "Water filling time" << endl;
+        } else {
+            cout << "Not now" << endl;
+        }
+    }
+    return 0;
+}
